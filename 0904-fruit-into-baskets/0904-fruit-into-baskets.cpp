6c86aab81245ac2046ff2ff85fa3cd26d365d278
Vishnu Kumar <visnhukumar.aec.cs23@sgei.org>
2026-08-20 16:45:59 +0530

    Time: 52 ms (22.93%), Space: 84.9 MB (34.54%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        map<int,int> baskets;   
+        unordered_map<int,int> baskets;   
         int left = 0, maxLen = 0;
 
         for (int right = 0; right < fruits.size(); right++) {
